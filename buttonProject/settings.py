6a211fe8086b5f671b31515b6a2d1efe492d71c2
xUncnny <62380601+xUncnny@users.noreply.github.com>
2020-04-01 15:33:01 +0300

    .

--- buttonProject/settings.py
+++ buttonProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -121,5 +121,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-# Activate Django-Heroku.
-django_heroku.settings(locals())

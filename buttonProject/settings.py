913c476eb9f0d34430dcc7be5b5a515f03befca6
xUncnny <62380601+xUncnny@users.noreply.github.com>
2020-04-01 17:36:23 +0300

    Update settings.py

--- buttonProject/settings.py
+++ buttonProject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3a%$s^!*kxb5=%qhd&=5y$zvbhiv=$#pzt+xgmjtp02f=k2bar'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['104.214.104.225','*']
 

baa500099be867ce7f00276d1308f12627d417d9
xUncnny <62380601+xUncnny@users.noreply.github.com>
2020-04-01 18:03:58 +0300

    ..

--- buttonProject/views.py
+++ buttonProject/views.py
@@ -32,19 +32,7 @@ def index(request):
 
     base_dir = os.path.dirname(
         os.path.dirname(os.path.realpath(__file__)).replace("\\", "/"))
-    ###########################
-    #for deleting the media files before start#
-    filemedia = base_dir + "/media"
-    folder = filemedia
-    for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception  as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+  
 
     ###########################
     #reading the file
